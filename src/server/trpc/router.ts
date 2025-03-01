714b667796d57d4d4a199246be9e79848c75493c
Steve (Binh Nguyen) <binh.nguyen@i-on.net>
2025-03-01 10:26:40 +0700

    Step 1: Update Restaurant, Category, Feature models with mockup datas

--- src/server/trpc/router.ts
+++ src/server/trpc/router.ts
@@ -1,24 +1,10 @@
-// import { router } from "./trpc";
-// import { restaurantRouter } from "./restaurant";
-
-// export const appRouter = router({
-//   restaurant: restaurantRouter,
-// });
-
-// export type AppRouter = typeof appRouter;
-
-
 import { publicProcedure, router } from './appRouter';
 import { z } from 'zod';
 import { restaurantRouter } from './restaurant'; 
 
-console.log("3: appRouter is being initialized!"); // Debugging log
-
-
 export const appRouter = router({
   hello: publicProcedure.input(z.object({ name: z.string().optional() }))
     .query(({ input }) => {
-      console.log('5: tRPC Response:', { message: `Hello, ${input.name ?? 'World'}!` });
       return { message: `Hello, ${input.name ?? 'World'}!` };
     }),
 
@@ -27,7 +13,6 @@ export const appRouter = router({
   //   console.log('🎯 tRPC Query Executed:', { message: 'Hello, World!' });
   //   return { message: 'Hello, World!' };
   // }),
-  
 });
 
 export type AppRouter = typeof appRouter;
