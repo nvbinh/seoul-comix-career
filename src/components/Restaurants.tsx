bda32fc2d31b8221ec9b0170d09941a4499390f3
Steve (Binh Nguyen) <binh.nguyen@i-on.net>
2025-03-03 15:43:05 +0700

    Update issues

--- src/components/Restaurants.tsx
+++ src/components/Restaurants.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 import { trpc } from "@/utils/trpc";
 import Image from "next/image";
 import ImageSlider from "@/components/ImageSlider";
@@ -27,17 +27,34 @@ const Restaurants: React.FC<{ category: string; name: string }> = ({
   );
   const [isLoading, setIsLoading] = useState(false);
 
-  const { data, isLoading: queryLoading, refetch } =
-    trpc.restaurant.getRestaurants.useQuery({
+  const queryParams = useMemo(
+    () => ({
       ...(category ? { categoryValue: category } : {}),
       name,
-    });
+    }),
+    [category, name]
+  );
+
+  const {
+    data,
+    isLoading: queryLoading,
+    refetch,
+  } = trpc.restaurant.getRestaurants.useQuery(queryParams, {
+    enabled: !category, // Only enable the query if category is provided
+  });
 
   const addFavoriteMutation = trpc.restaurant.addFavorite.useMutation({
     onSuccess: () => {
       refetch();
     },
   });
+
+  const handleAddFavorite = useCallback(
+    (id: string) => {
+      addFavoriteMutation.mutate(id);
+    },
+    [addFavoriteMutation]
+  );
 
   useEffect(() => {
     if (data && !queryLoading) {
@@ -55,10 +72,6 @@ const Restaurants: React.FC<{ category: string; name: string }> = ({
 
   if (isLoading) return <p>Loading...</p>;
 
-  const handleAddFavorite = (id: string) => {
-    addFavoriteMutation.mutate(id);
-  };
-
   return restaurants?.map((res) => (
     <div key={res.id} className="py-4">
       <div className="bg-white rounded-lg overflow-hidden relative">
@@ -74,7 +87,9 @@ const Restaurants: React.FC<{ category: string; name: string }> = ({
         )}
         {res.images.length > 1 && <ImageSlider images={res.images} />}
         <button
-          className={`absolute top-4 right-4 bg-opacity-50 rounded-full text-white font-bold p-2 shadow-md ${res.isFavorite ? "bg-red-500" : "bg-gray-500"}`}
+          className={`absolute top-4 right-4 bg-opacity-50 rounded-full text-white font-bold p-2 shadow-md ${
+            res.isFavorite ? "bg-red-500" : "bg-gray-500"
+          }`}
           onClick={() => handleAddFavorite(res.id)}
         >
           <svg
@@ -88,19 +103,21 @@ const Restaurants: React.FC<{ category: string; name: string }> = ({
           </svg>
         </button>
         <div className="p-4">
-          <h2 className="text-orange-500 text-sm font-bold">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-stars"
-              viewBox="0 0 16 16"
-            >
-              <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
-            </svg>
-            나카노시마×야키토리 상위 맛집
-          </h2>
+          {res.isFavorite && (
+            <h2 className="text-orange-500 text-sm font-bold flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-stars"
+                viewBox="0 0 16 16"
+              >
+                <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
+              </svg>
+              나카노시마×야키토리 상위 맛집
+            </h2>
+          )}
           <div className="flex items-center justify-between">
             <h1 className="text-lg font-semibold max-w-xs truncate ...">
               {res.name}
